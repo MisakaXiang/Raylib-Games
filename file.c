0d8658d526ea5cf0a3a124c7b1551c060463bf81
MisakaXiang <975737669@qq.com>
2021-08-23 12:50:15 +0800

    style:补充了注释，增加了file.c

---
+++ file.c
@@ -0,0 +1,49 @@
+#include "file.h"
+
+int WriteSoulsToFile(int song_id, int player_position_x) 
+{
+    char SaveSoulFileName[10];
+    if (song_id < 1 || song_id > 4)
+    {
+        fprintf(stderr, "song_id invaild.\n");
+        return SONG_ID_INVAILD;
+    }
+    sprintf(SaveSoulFileName, "save_song%d.txt", song_id);
+    FILE *fpWrite = fopen(SaveSoulFileName, "w");
+    if (!fpWrite)
+    {
+        fprintf(stderr, "write souls to file error.\n");
+        return NULL_POINTER_ERROR;
+    }
+    fprintf(fpWrite, "%lf\n", player_position_x);
+    fclose(fpWrite);
+    return SUCCESS;
+}
+
+int ReadSoulsFromFile(int song_id, Soul *souls,int *p_note_num)
+{
+    // read from note file and create notes
+    char soulFileName[10];
+    if (song_id < 1 || song_id > 4)
+    {
+        fprintf(stderr, "song_id invaild.\n");
+        return SONG_ID_INVAILD;
+    }
+    sprintf(soulFileName, "song%d.txt", song_id);
+    FILE *fpRead = fopen(soulFileName, "rb");
+    if (!fpRead)
+    {
+        fprintf(stderr, "Song souls file read error.\n");
+        return NULL_POINTER_ERROR;
+    }
+    double soul_temp;
+    *p_note_num = 0; // 这首歌曲的音符数
+    // 将文件中的音符全部读入到souls数组中
+    while (fscanf(fpRead, "%lf", &soul_temp) == 1)
+    {
+        souls[*p_note_num].rect.x = (int)soul_temp;
+        *p_note_num += 1;
+    }
+    fclose(fpRead);
+    return SUCCESS;
+}
--- updateplayer.h
+++ updateplayer.h
@@ -5,7 +5,7 @@
 #include "memtools.h"
 #include "interface.h"
 
-// 用来保存已经播放过的soul索引
+// 用来保存已经播放过的 soul 索引
 int pass_soul_index = 0;
 
 // CheckIfSoulsEnd 判断 player 是否跑到最后一个音符, 若是, 停止播放音乐, 回到结束界面
@@ -17,8 +17,8 @@ void CheckIfGameOver(Player *pPlayer, EnvItem **pEnvBlocks);
 // CheckIfSoulsPass 判断 音符 是否已经播放 (pass)
 void CheckIfSoulsPass(Player *pPlayer, Soul **pSouls);
 
-// CheckIfJumpOnBlocks 判断 player 是否会跳上envblock, 若能跳上，则更新player的y轴速度以及y轴位置
-void CheckIfJumpOnBlocks(Player *pPlayer, EnvItem **pEnvBlocks, int *JumpOnBlocks, float delta);
+// CheckCanJumpOnBlocks 判断 player 是否会跳上envblock, 若能跳上，则更新player的y轴速度以及y轴位置
+void CheckCanJumpOnBlocks(Player *pPlayer, EnvItem **pEnvBlocks, int *JumpOnBlocks, float delta);
 
 // ComputeScore 判定分数
 void ComputeScore(Player *pPlayer, Soul **pSouls);
@@ -28,3 +28,6 @@ void UpdatePlayerRect(Player *pPlayer);
 
 // UpdatePlayerPosition 更新 player 的位置
 void UpdatePlayerPosition(Player *pPlayer, int *JumpOnBlocks, float delta);
+
+// ComputeDistance 计算 player 和 soul 的距离函数
+int ComputeDistance(Player *pPlayer, Soul *pSouls);
