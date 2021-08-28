7dff5e48b40b53a859782136703858c447a504c9
KarKLi <kark_li@qq.com>
2021-08-28 21:01:24 +0800

    feat: 新增了判空指针函数

--- memtools.h
+++ memtools.h
@@ -3,8 +3,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #ifdef __linux__
-    #include <execinfo.h>
+#include <execinfo.h>
 #endif
 
 #include "errcode.h"
@@ -30,6 +31,34 @@ void CheckPointerWithExit(void *ptr)
     }
 }
 
+// CheckPtrWithMsg 检查指针是否为空，为空时打印debug信息。
+int CheckPtrWithMsg(void *ptr,
+                         const char *file,
+                         int line,
+                         const char *msg)
+{
+    if (CheckPointer(ptr) != NULL_POINTER_ERROR)
+    {
+        return;
+    }
+    if (msg[strlen(msg) - 1] == "\n")
+    {
+        fprintf(stderr, "%s:%d %s", file, line, msg);
+    }
+    fprintf(stderr, "%s:%d %s\n", file, line, msg);
+    return NULL_POINTER_ERROR;
+}
+
+// CheckPtrWithMsgExit 检查指针是否为空，为空时打印debug信息并退出。
+void CheckPtrWithMsgExit(void *ptr,
+                     const char *file,
+                     int line,
+                     const char *msg)
+{
+    CheckPtrWithMsg(ptr, file, line, msg);
+    exit(NULL_POINTER_ERROR);
+}
+
 // PrintBacktrace 打印当前调用栈
 #ifdef __linux__
 void PrintBacktrace()
@@ -40,13 +69,14 @@ void PrintBacktrace()
     int nptrs = backtrace(buffer, SIZE);
     strings = backtrace_symbols(buffer, nptrs);
 
-    if (strings == NULL) {
+    if (strings == NULL)
+    {
         perror("backtrace_symbols");
         exit(NULL_POINTER_ERROR);
     }
 
     fprintf(stderr, "Current Backtrace:\n");
-    for (int j = 0; j < nptrs; j++) 
+    for (int j = 0; j < nptrs; j++)
     {
         fprintf(stderr, "%s\n", strings[j]);
     }
